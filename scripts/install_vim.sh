1c5ca66ba000074fed507032d9fe678e152eea8c
chipkajb <jbc274@cornell.edu>
2020-08-07 14:38:17 -0400

    improved seamless use of install_vim script

--- scripts/install_vim.sh
+++ scripts/install_vim.sh
@@ -2,10 +2,8 @@
 
 # DIRECTIONS:
 # 1. Run this script (./install_vim.sh)
-# 2. Open up ~/.vimrc
-# 3. Install all plugins (:PluginInstall)
-# 4. Make sure libclang is installed (sudo apt-get install -y libclang-dev)
-# 5. Change the path to libclang.so to the proper path in ~/.vimrc
+# 2. Make sure libclang is installed (sudo apt-get install -y libclang-dev)
+# 3. Change path to libclang.so in ~/.vimrc
 
 rm -rf /home/$USER/.vim
 rm -rf /home/$USER/.vimrc
@@ -15,3 +13,11 @@ cp ../config/.vimrc /home/$USER 2>/dev/null
 
 rm -rf /home/$USER/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git /home/$USER/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall > /dev/null
+
+printf "\nVIM setup complete."
+printf "\nRemember to change path to libclang.so in ~/.vimrc"
+printf "\nCurrent path is:\n"
+cat ~/.vimrc | grep libclang.so | awk '{print $2}' | cut -c 23- | head -c-2
+printf "\nCorrect path is:\n"
+find /usr/lib/ -iname libclang.so
