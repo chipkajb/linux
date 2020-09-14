cdcff4c7962ac0d1d6ffce2c815deba2d22d2671
chipkajb <jbc274@cornell.edu>
2020-09-14 15:18:36 -0400

    fixed bugs in images2video converter

--- python/convert_images2video.py
+++ python/convert_images2video.py
@@ -3,12 +3,13 @@ import numpy as np
 import glob
 import pdb
 import argparse
+import pdb
 
 parser = argparse.ArgumentParser()
-parser.add_argument("image_dir", type=str, help="path to image directory (ex. ~/images)")
-parser.add_argument("ext", type=str, help="image file extension (ex. jpg)")
-parser.add_argument("out", type=str, help="output video, must be avi (ex. out.avi)")
-parser.add_argument("fps", type=int, help="output fps (ex. 20)")
+parser.add_argument("--image_dir", type=str, help="path to image directory (ex. ~/images)")
+parser.add_argument("--ext", type=str, help="image file extension (ex. jpg)")
+parser.add_argument("--out", type=str, help="output video, must be avi (ex. out.avi)")
+parser.add_argument("--fps", type=int, help="output fps (ex. 20)")
 args = parser.parse_args()
 
 image_dir = args.image_dir
@@ -25,16 +26,16 @@ for filename in images:
     height, width, layers = img.shape
     size = (width,height)
     img_array.append(img)
-    print("Processing frame " + str(count) + "/" + str(len(images)-1))
+    print("Pre-processing frame " + str(count) + "/" + str(len(images)-1))
     count = count + 1
 
-print("")
-print("Saving video to: " + out)
-print("")
+print("\nSaving video to: " + out + "\n")
 outVideo = cv2.VideoWriter(out,cv2.VideoWriter_fourcc(*'DIVX'), fps, size)
 count = 0
 
 for i in range(len(img_array)):
     outVideo.write(img_array[i])
-    print("Processing frame " + str(count) + "/" + str(len(img_array)-1))
-    outVideo.release()
+    print("Writing frame " + str(count) + "/" + str(len(img_array)-1))
+    count = count + 1
+outVideo.release()
+print("\nSaved video to: " + out)
