a040b106f5ef149742b18fc7058acec162debf48
chipkajb <jbc274@cornell.edu>
2023-06-04 00:07:25 -0400

    add neovim

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,3 +12,21 @@ chsh -s $(which zsh) # must enter password here
 
 # install oh my zsh
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+
+# install neovim
+wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+sudo mv ./nvim.appimage /usr/bin
+sudo chmod 764 /usr/bin/nvim.appimage
+
+# install nerd font
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.1/Mononoki.zip
+mkdir Mononoki
+mv Mononoki.zip Mononoki
+cd Mononoki && unzip Mononoki.zip && rm Mononoki.zip && cd ..
+mv Mononoki ~/.fonts
+fc-cache -fv
+
+# install NvChad
+mv ~/.config/nvim ~/.config/nvim.backup 2> /dev/null
+rm -rf ~/.local/share/nvim/
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
