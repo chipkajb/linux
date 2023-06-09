90edd6ddc24e33d9dc437a351e62d5b120df528e
chipkajb <jbc274@cornell.edu>
2023-06-09 10:56:59 -0400

    make the require function work regardless of starting directory

--- config/nvim/custom/init.lua
+++ config/nvim/custom/init.lua
@@ -3,6 +3,7 @@ require("custom.set")
 
 -- Delayed loading of Lua file
 vim.defer_fn(function()
-    require('after.plugin.tmux_navigator')
+    local path = vim.fn.stdpath('config') .. '/after/plugin/tmux_navigator.lua'
+    dofile(path)
 end, 0)
 
