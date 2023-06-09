0e326476274a90389c2caedcb122905065dde4ee
chipkajb <jbc274@cornell.edu>
2023-06-09 13:42:42 -0400

    split window remaps

--- config/nvim/after/remap.lua
+++ config/nvim/after/remap.lua
@@ -1,2 +1,6 @@
 --- remove search highlight
 vim.keymap.set("n", "//", ":nohl<CR>", {desc="Remove search highlights"})
+
+--- split buffer
+vim.keymap.set("n", "<leader>v", ":vnew<CR>", {silent=true, desc="Split buffer vertically"})
+vim.keymap.set("n", "<leader>h", ":new<CR>", {silent=true, desc="Split buffer horizontally"})
