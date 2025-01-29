a9ef39524a7905d5701bdc4348e6e0364f0b2968
chipkajb <jbc274@cornell.edu>
2025-01-29 10:33:32 -0500

    install ruff

--- setup.sh
+++ setup.sh
@@ -162,6 +162,7 @@ install_vscode() {
     code --install-extension tickleforce.scrolloff
     code --install-extension vscodevim.vim
     code --install-extension Ransh.ransh
+    code --install-extension charliermarsh.ruff
     ln -sfn $PWD/config/vscode/settings.json ~/.config/Code/User/
     ln -sfn $PWD/config/vscode/keybindings.json ~/.config/Code/User/
     printf "${GREEN}DONE${NC} -- VS Code installed to ${YELLOW}$(which code)${NC} -- ${YELLOW}v$(code --version | head -n 1)${NC}\n"
