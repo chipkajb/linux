1c4f19deee01022002dba1c19b41deea2b895bd0
chipkajb <jbc274@cornell.edu>
2025-01-31 13:32:14 -0500

    add cwd to pythonpath and restore alias

--- scripts/python_startup.py
+++ scripts/python_startup.py
@@ -5,10 +5,11 @@ import os
 python_stdlib = os.environ.get("PYTHON_STDLIB")
 python_site_packages = os.environ.get("PYTHON_SITE_PACKAGES")
 python_lib_dynload = os.environ.get("PYTHON_LIB_DYNLOAD")
+cwd = os.environ.get("PWD")
 
 if python_stdlib and python_site_packages and python_lib_dynload:
     # Override sys.path to remove system-level Python paths
-    sys.path = [python_stdlib, python_site_packages, python_lib_dynload]
+    sys.path = [cwd, python_stdlib, python_site_packages, python_lib_dynload]
 
     # Force Python to use the correct stdlib location
     sys.prefix = python_stdlib
@@ -18,10 +19,3 @@ if python_stdlib and python_site_packages and python_lib_dynload:
     # Force Python's built-in module loader to look in the correct stdlib
     sys.modules["os"].__file__ = os.path.join(python_stdlib, "os.py")
     sys.modules["sys"].__file__ = os.path.join(python_stdlib, "sys.py")
-
-# # Debugging output
-# print("sys.prefix:", sys.prefix)
-# print("sys.base_prefix:", sys.base_prefix)
-# print("sys.exec_prefix:", sys.exec_prefix)
-# print("sys.path:", sys.path)
-# print("os.__file__:", sys.modules["os"].__file__)
