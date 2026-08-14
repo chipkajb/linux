70bc3f7d3ae8e6f58f5dd82b8ea3bf5443ef5636
chipkajb <jbc274@cornell.edu>
2026-08-14 15:54:45 -0400

    fix vim errors/warnings

--- config/nvim/custom/configs/null-ls.lua
+++ config/nvim/custom/configs/null-ls.lua
@@ -1,14 +1,16 @@
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-local null_ls = require('null-ls')
+local null_ls = require("null-ls")
+
+-- Prefer project venv tools so mypy picks up plugins like pydantic.mypy.
+local venv_bin = ".venv/bin"
 
 local opts = {
     sources = {
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.diagnostics.mypy,
-        null_ls.builtins.diagnostics.ruff,
+        null_ls.builtins.formatting.black.with({ prefer_local = venv_bin }),
+        null_ls.builtins.diagnostics.mypy.with({ prefer_local = venv_bin }),
     },
     on_attach = function (client, bufnr)
-        if client.supports_method("textDocument/formatting") then
+        if client:supports_method("textDocument/formatting") then
             vim.api.nvim_clear_autocmds({
                 group = augroup,
                 buffer = bufnr,
